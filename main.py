b1c78c5c1e4352a6830162c9fdaa0a63b2a3f003
Pedro Cotta Badaro <37852363+pedrocotta98@users.noreply.github.com>
2020-11-10 19:23:49 -0300

    Fix funcao transfere, teste dos operadores

--- main.py
+++ main.py
@@ -11,23 +11,44 @@ Doscente: Saulo Moraes
 """
 from graph import Node, Graph, Edge
 from jug import Jug
+import copy
 
 def main():
     """
     Main function of application
     """
+
+    """
+        OPERACOES
+        r1=ENCHER JARRO
+        r2=ESVAZIAR JARRO
+        r3=TRANSFERIR ESQUERDA
+        r4= TRANSFERIR DIREITA
+    """
     jug_array = [Jug(4),Jug(3)]
+
     jug_array_2 = [Jug(4),Jug(3)]
     jug_array_2[0].fill()
 
+    jug_array_3 = copy.deepcopy(jug_array_2)
+    jug_array_3[0].transfer_to(jug_array_3[1])
+
+    jug_array_4 = copy.deepcopy(jug_array_3)
+    jug_array_4[1].spill()
+
+
+    
+
     n = Node(None, jug_array)
     n2 = Node(n, jug_array_2)
-    n3 = Node(n, jug_array_2)
+    n3 = Node(n2, jug_array_3)
+    n4 = Node(n3, jug_array_4)
 
     g = Graph()
     g.insert_node(n,"begin")
     g.insert_node(n2, "r1")
-    g.insert_node(n2, "r1")
+    g.insert_node(n3, "r4")
+    g.insert_node(n4, "r2")
 
     g.print_graph()
 
